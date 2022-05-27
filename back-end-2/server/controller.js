67bb219f5e46412ff4d10147487a4a5d007791cb
Chris McKnight <tenlongfingers10@gmail.com>
2022-05-26 21:55:33 -0600

    Most of the controller built out

---
+++ back-end-2/server/controller.js
@@ -0,0 +1,13 @@
+const getHouses = (res, req) => {};
+const deletHouse = (res, req) => {};
+const createHouse = (res, req) => {};
+//app.post(`${baseURL}/api/new-house, (req, res) => {
+//    let { address, price, img} = req.body
+//
+// if (!address || !price ||) {
+// res.status(400).send(`Please include at least the address and the price of the house`)
+//} else {
+// houseDatabase.push(req.body)}
+//res.status(200).send(`Congratulations! Your house is now officially for sale!`)
+//})
+const updateHouse = (res, req) => {};
--- back-end-2/server/index.js
+++ back-end-2/server/index.js
@@ -1,5 +1,9 @@
 const express = require(`express`);
 const cors = require(`cors`);
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const PORT = 4004;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
